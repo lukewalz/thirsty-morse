ee562e56b52e2a93f6620071cfec9ec921817dc7
lukewalz <lukewalz1@gmail.com>
2020-10-16 00:34:17 -0500

    stuff

--- src/Matchup.js
+++ src/Matchup.js
@@ -8,14 +8,13 @@ import { Row, Col, Badge, CardTitle } from 'reactstrap';
 
 function Matchup({ ...props }) {
     return (
-        console.log(props),
         props.game ?
             <div>
                 <Row>
                     <Col>
                         <CardTitle>{props.game.away_team}</CardTitle>
                         <div>
-                            {props.game.logos ? <><img alt={props.game.away_team} src={props.game?.logos[1][0]?.logos[0]} /><Badge color={props.game.home_points < props.game.away_points ? 'success' : 'danger'}>{props.game.away_points}</Badge></> : []}
+                            {props.game.logos ? <><img alt={props.game.away_team} src={props.game?.logos[1][0]?.logos[0].replace('http', 'https')} /><Badge color={props.game.home_points < props.game.away_points ? 'success' : 'danger'}>{props.game.away_points}</Badge></> : []}
                         </div>
                     </Col>
 
@@ -23,7 +22,7 @@ function Matchup({ ...props }) {
                         <CardTitle>{props.game.home_team}</CardTitle>
 
                         <div>
-                            {props.game.logos ? <><img alt={props.game.home_team} src={props.game?.logos[0][0]?.logos[0]} /><Badge color={props.game.home_points > props.game.away_points ? 'success' : 'danger'}>{props.game.home_points}</Badge></> : []}
+                            {props.game.logos ? <><img alt={props.game.home_team} src={props.game?.logos[0][0]?.logos[0].replace('http', 'https')} /><Badge color={props.game.home_points > props.game.away_points ? 'success' : 'danger'}>{props.game.home_points}</Badge></> : []}
                         </div>
                     </Col>
                 </Row>
