5b5cb3d6f39aead934dbcf54e9f94c11f2335c7a
lukewalz <lukewalz1@gmail.com>
2020-12-02 15:50:39 -0600

    sdfdsf

---
+++ build/precache-manifest.4e8211d5cc9b8ebb5fd6afd63e12609a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c49ffa4b684f3e9e09b13b1ec6fd9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3b1cabde47793007d3f",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "49903193d1463fda8f90",
+    "url": "/static/css/main.b52f97b9.chunk.css"
+  },
+  {
+    "revision": "a3b1cabde47793007d3f",
+    "url": "/static/js/2.f4c25c67.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f4c25c67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49903193d1463fda8f90",
+    "url": "/static/js/main.b3119741.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/static/media/logo.8dad8028.svg"
+  }
+]);
