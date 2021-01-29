0f7b52832216b4a6a355b0882beb566aa590f875
lukewalz <lukewalz1@gmail.com>
2021-01-29 16:40:56 -0600

    added sentry to webapp

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import ReactDOM from "react-dom";
 import App from "./App";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://42a79955943a43b5bfac3e08782eae65@o247578.ingest.sentry.io/5615152",
+    integrations: [
+        new Integrations.BrowserTracing(),
+    ],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
