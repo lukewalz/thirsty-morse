8fd5c1228d9e5072e2f702c522f01585d196b399
lukewalz <lukewalz1@gmail.com>
2021-02-03 16:17:11 -0600

    sfdsf

--- src/subscriptions.js
+++ src/subscriptions.js
@@ -1,8 +1,21 @@
 
 import axios from "axios";
 
+const vapidPublicKey = 'BGuyWcyfyg50-ixydbkI2AEeFDFJvHhN-5xD4c1bGz5zWySLs1Zh8d3HZPoAxnSyWkAKn-erIqSN-5l1a6jZbTQ'
+const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey)
 
-const path = process.env.NODE_ENV === 'development' ? 'http://localhost:9000/.netlify/functions/server/notifications/register' : '/.netlify/functions/server/notifications/register'
+function urlBase64ToUint8Array(base64String) {
+    const padding = "=".repeat((4 - base64String.length % 4) % 4)
+    const base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/")
+
+    const rawData = window.atob(base64)
+    const outputArray = new Uint8Array(rawData.length)
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i)
+    }
+    return outputArray
+}
 
 export default function subscribePush() {
     navigator.serviceWorker.ready.then(registration => {
@@ -10,14 +23,12 @@ export default function subscribePush() {
             alert("Push Unsupported")
             return
         }
-
-        return registration.pushManager.getSubscription().then(y => {
-            console.log(y);
-            registration.pushManager
-                .subscribe(y.options)
-                .then(subscription => axios.post(path, subscription))
-                .catch(err => console.error("Push subscription error: ", err))
-        })
-
+        registration.pushManager
+            .subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: convertedVapidKey
+            })
+            .then(subscription => axios.post("http://localhost:9000/.netlify/functions/server/notifications/register", subscription))
+            .catch(err => console.error("Push subscription error: ", err))
     })
 }
