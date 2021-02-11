ff61c0fd9567321bb278d20c5478f1fcffc35e9b
lukewalz <lukewalz1@gmail.com>
2021-02-10 21:07:23 -0600

    trying to figure out notifications;

--- express/routes/register.js
+++ express/routes/register.js
@@ -19,6 +19,7 @@ let pushIntervalID
 
 
 router.post("/register", (req, res, next) => {
+    console.log('sfsdfsd---------------------------------------------')
     subscription = req.body
     res.sendStatus(201)
     pushIntervalID = setInterval(() => {
