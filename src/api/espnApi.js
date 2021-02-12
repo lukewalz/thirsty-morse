f7e3fac62fe686fc941da9e27ad6722587a21336
lukewalz <lukewalz1@gmail.com>
2021-02-11 19:15:14 -0600

    fixed some issues with wins not being displaey and addded get games by id

--- src/api/espnApi.js
+++ src/api/espnApi.js
@@ -37,4 +37,24 @@ export async function getGames(league) {
     })
 }
 
+export async function getGameById(league, game_id) {
+    var gameOrMatch = league === 'soccer' ? 'match' : 'game';
+
+    var game = await fetch('https://secure.espn.com/core/' + league + '/' + gameOrMatch + '?gameid=' + game_id + '&xhr=1&v=' + Date.now()).then(e => e.json()).then(f => f.gamepackageJSON)
+        .then(g => {
+
+            var game = {
+                competitors: g.header.competitions[0].competitors,
+                odds: league === 'soccer' ? g.odds[g.odds.findIndex(e => e.details && e.overUnder)] : g.pickcenter[g.pickcenter.findIndex(e => e.details && e.overUnder)],
+                date: g.header.competitions[0].date,
+                status: g.header.competitions[0].status,
+                id: g.header.id,
+                lastPlay: g.commentary ? g.commentary[g.commentary.length - 1]?.text : g.plays ? g.plays[g.plays.length - 1]?.text : '',
+                boxScore: g.boxscore.teams
+            };
+            return game;
+        })
+    return game;
+}
+
 
--- src/pages/Matchup.js
+++ src/pages/Matchup.js
@@ -76,7 +76,7 @@ function Matchup({ placeWager, user, ...props }) {
                     <Col>
                         <div className='teamSection'>
                             <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-                                {wagers.length > 0 ? wagers.map((e, i) => e.selection.split('@')[0] === team1Abbreviation ? <Chip label={e.selection} style={{ backgroundColor: '#8bc34a' }} key={i} onClick={g => { g.stopPropagation(); handleWagerClick(e) }} /> : '') : ''}
+                                {wagers.length > 0 ? wagers.map((e, i) => e.selection.split('@')[0] === team1Abbreviation ? <Chip label={e.status !== 'final' ? '$' + e.amount : e.outcome} style={{ backgroundColor: '#8bc34a' }} key={i} onClick={g => { g.stopPropagation(); handleWagerClick(e) }} /> : '') : ''}
                             </div>
                             <div>
                                 <div style={{
@@ -104,7 +104,7 @@ function Matchup({ placeWager, user, ...props }) {
                         </div>
                         <div className='teamSection'>
                             <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-                                {wagers.length > 0 ? wagers.map((e, i) => e.selection.split('@')[0] === team2Abbreviation ? <Chip label={e.selection} style={{ backgroundColor: '#8bc34a' }} key={i} onClick={g => { g.stopPropagation(); handleWagerClick(e) }}>{e.status !== 'final' ? '$' + e.amount : e.outcome}</Chip> : '') : ''}
+                                {wagers.length > 0 ? wagers.map((e, i) => e.selection.split('@')[0] === team2Abbreviation ? <Chip label={e.status !== 'final' ? '$' + e.amount : e.outcome} style={{ backgroundColor: '#8bc34a' }} key={i} onClick={g => { g.stopPropagation(); handleWagerClick(e) }} /> : '') : ''}
                             </div>
                             <div>
                                 <div style={{
