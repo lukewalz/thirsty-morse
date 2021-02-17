283b30eeee5585557cab32a8d5b442a59db2b26b
lukewalz <lukewalz1@gmail.com>
2021-02-16 22:37:24 -0600

    removed caching

--- public/sw.js
+++ public/sw.js
@@ -13,36 +13,3 @@ self.addEventListener("push", event => {
 
   event.waitUntil(self.registration.showNotification(title, body))
 })
-
-self.addEventListener("install", event => {
-  console.log('Service worker installed');
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache)
-      })
-  )
-})
-
-self.addEventListener("activate", event => {
-  console.log('Service worker activated')
-})
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request)
-      })
-  )
-})
-
-const CACHE_NAME = 'my-site-cache-v1';
-const urlsToCache = [
-  '/',
-  '/home',
-  '/static/js/bundle.js',
-  '/static/js/0.chunk.js',
-  '/static/js/main.chunk.js'
-]
-
