7f036153fe5d06295914dc8865ef161cd8d6f505
lukewalz <lukewalz1@gmail.com>
2021-02-18 21:53:43 -0600

    game or match error

--- express/helpers/common.js
+++ express/helpers/common.js
@@ -136,7 +136,7 @@ async function getGameById(wager) {
     var currentLine;
 
     if (wager.sport === 'soccer') {
-        apiPath = 'https://secure.espn.com/core/' + wager.sport + '/' + gameOrMatch + '?gameid=' + id + '&xhr=1';
+        apiPath = 'https://secure.espn.com/core/' + wager.sport + '/match?gameid=' + id + '&xhr=1';
     }
 
     await fetch(apiPath).then(e => e.json()).catch(er => er).then(r => {
