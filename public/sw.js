481d2db4c15b52ad1de642625ad6b4773bc23313
lukew <lukewalz1@gmail.com>
2021-03-11 15:30:01 -0600

    offile

--- public/sw.js
+++ public/sw.js
@@ -13,3 +13,35 @@ self.addEventListener("push", event => {
 
   event.waitUntil(self.registration.showNotification(title, body))
 })
+
+var CACHE_NAME = 'thirsty-cache';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+      )
+  );
+});
