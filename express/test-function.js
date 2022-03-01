d475ecd855a9215f6a1a0ad43fb2825a75377e80
lukew <lukewalz1@gmail.com>
2022-02-28 21:36:43 -0600

    asdad

--- express/test-function.js
+++ express/test-function.js
@@ -1,10 +1,13 @@
-exports = async function run() {
+const { schedule } = require('@netlify/functions')
+const handler = async function run() {
   const response = await context.http.get({
-      headers: {
-        'x-auth-token':['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDRhOTI1NWU2ODg0MDAwMDkyYjhjYmUiLCJpYXQiOjE2MzA3MDA0MTh9.9WAk3qG_a7FdcOpUjVjCDgWFfjhFs2-j5ugBFcax45E']
-      },
-      url: "https://thirsty-morse-d0f09c.netlify.app/.netlify/functions/server/wagers/admin",
+    headers: {
+      'x-auth-token': ['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDRhOTI1NWU2ODg0MDAwMDkyYjhjYmUiLCJpYXQiOjE2MzA3MDA0MTh9.9WAk3qG_a7FdcOpUjVjCDgWFfjhFs2-j5ugBFcax45E']
+    },
+    url: "https://thirsty-morse-d0f09c.netlify.app/.netlify/functions/server/wagers/admin",
   });
-  
+
   return EJSON.stringify(response.body);
 };
+
+module.exports.handler = schedule("@hourly", handler);
