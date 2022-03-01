9a425c9db6def420092549b1bbaae160a31a740a
lukew <lukewalz1@gmail.com>
2022-02-28 22:04:00 -0600

    asdad

--- express/test-function.js
+++ express/test-function.js
@@ -1,5 +1,5 @@
 const { schedule } = require('@netlify/functions')
-const handler = async function run() {
+const handler = async function (event, context) {
   const response = await context.http.get({
     headers: {
       'x-auth-token': ['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDRhOTI1NWU2ODg0MDAwMDkyYjhjYmUiLCJpYXQiOjE2MzA3MDA0MTh9.9WAk3qG_a7FdcOpUjVjCDgWFfjhFs2-j5ugBFcax45E']
