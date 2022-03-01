3de2210702e6e24f0a57ed9748ebebe7101fafbf
lukew <lukewalz1@gmail.com>
2022-02-28 23:09:16 -0600

    sdf

--- express/test-function.js
+++ express/test-function.js
@@ -5,7 +5,7 @@ const fetch = require("node-fetch");
 const handler = async function (event, context) {
   const response = await fetch("https://thirsty-morse-d0f09c.netlify.app/.netlify/functions/server/wagers/admin", {
     headers: {
-      'x-auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDRhOTI1NWU2ODg0MDAwMDkyYjhjYmUiLCJpYXQiOjE2MzA3MDA0MTh9.9WAk3qG_a7FdcOpUjVjCDgWFfjhFs2-j5ugBFcax45E']
+      'x-auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDRhOTI1NWU2ODg0MDAwMDkyYjhjYmUiLCJpYXQiOjE2MzA3MDA0MTh9.9WAk3qG_a7FdcOpUjVjCDgWFfjhFs2-j5ugBFcax45E'
     },
   });
 
