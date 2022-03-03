ab056b01cd55d771151fc81eef953cd3d0008aed
Luke Walz <luke.walz@opendorse.com>
2022-03-03 15:53:03 -0600

    this

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,121 +1,127 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Toolbar, Button, Typography, Badge, Box, Chip, Paper } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Typography,
+  Box,
+  Chip,
+} from "@material-ui/core";
 import { connect } from "react-redux";
 import { logout, loadUpdatedWagers } from "../redux/actions/userActions";
-import subscribePush from '../subscriptions';
-
-
+// import subscribePush from "../subscriptions";
 
 function NavBar({ logout, user, loadUpdatedWagers }) {
-    const [balance, setBalance] = useState(0);
-    const [pending, setPending] = useState(0);
+  const [balance, setBalance] = useState(0);
+  const [pending, setPending] = useState(0);
 
-    useEffect(() => {
-        askUserPermission().then((e) => {
+  useEffect(() => {
+    // askUserPermission().then((e) => {
+    if (user.isAuthUser) {
+      //   subscribePush().then(() =>
+      loadUpdatedWagers().then(() => {
+        getBalance();
+        getPending();
+      });
+      //   );
+    }
+    // });
 
-            if (e === 'granted' && user.isAuthUser) {
-                subscribePush()
-                    .then(() => loadUpdatedWagers()
-                        .then(() => { getBalance(); getPending() }))
-            }
-        })
-
-        function getBalance() {
-            var balance = 0;
-            user.wagers && user.wagers.length > 0 ? user.wagers.filter(e => e.status === 'final').map(w => {
-                return balance += parseInt(w.result);
-            }) : setBalance(0);
-            setBalance(balance);
-        }
-
-        function getPending() {
-            var pending = 0;
-            user.wagers && user.wagers.length > 0 ? user.wagers.filter(e => e.status !== 'final').map(w => {
-                return pending += parseInt(w.amount);
-            }) : setBalance(0);
-            setPending(pending);
-        }
-
-        loadUpdatedWagers();
-
-    }, [user.wagers.length]);
-
-
-    function isPushNotificationSupported() {
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/sw.js')
-                    .then(reg => {
-
-                    })
-                    .catch(err => {
-
-                    })
+    function getBalance() {
+      var balance = 0;
+      user.wagers && user.wagers.length > 0
+        ? user.wagers
+            .filter((e) => e.status === "final")
+            .map((w) => {
+              return (balance += parseInt(w.result));
             })
-        }
+        : setBalance(0);
+      setBalance(balance);
     }
 
-    async function askUserPermission() {
-        if (typeof Notification !== 'undefined') {
-            return await Notification.requestPermission();
-        }
+    function getPending() {
+      var pending = 0;
+      user.wagers && user.wagers.length > 0
+        ? user.wagers
+            .filter((e) => e.status !== "final")
+            .map((w) => {
+              return (pending += parseInt(w.amount));
+            })
+        : setBalance(0);
+      setPending(pending);
     }
 
-    isPushNotificationSupported();
+    loadUpdatedWagers();
+  }, [loadUpdatedWagers, user.isAuthUser, user.wagers, user.wagers.length]);
 
+  // function isPushNotificationSupported() {
+  //     if ('serviceWorker' in navigator) {
+  //         window.addEventListener('load', () => {
+  //             navigator.serviceWorker.register('/sw.js')
+  //                 .then(reg => {
 
-    return (
-        <AppBar position='sticky'>
-            <Toolbar>
-                <Typography variant="h6">TMSB</Typography>
-                <div style={{ marginLeft: "auto" }}>
-                    {user.isAuthUser ? (
-                        <Box flexDirection='column'>
+  //                 })
+  //                 .catch(err => {
 
-                            <Link to="/home">
-                                <Button>Dashboard</Button>
-                            </Link>
-                            <Link to="/my-account">
-                                <Button>Account</Button>
-                            </Link>
-                            <Button onClick={logout}>
-                                Logout
-                            </Button>
+  //                 })
+  //         })
+  //     }
+  // }
 
-                        </Box>
-                    ) : (
-                        <Box>
-                            <Link to="/login">
-                                <Button>Login</Button>
-                            </Link>
-                            <Link to="/register">
-                                <Button>Register</Button>
-                            </Link>
-                        </Box>
-                    )}
-                </div>
-            </Toolbar>
-            <Box padding={.5} alignSelf='flex-end'>
-                <Chip color='secondary' label={`Account Balance: ${balance}`} />
-                <Chip color='secondary' label={`Max: 1000`} />
-                <Chip color='secondary' label={`Pending: ${pending}`} />
+  //   async function askUserPermission() {
+  //     if (typeof Notification !== "undefined") {
+  //       return await Notification.requestPermission();
+  //     }
+  //   }
+
+  //   isPushNotificationSupported();
+
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <Typography variant="h6">TMSB</Typography>
+        <div style={{ marginLeft: "auto" }}>
+          {user.isAuthUser ? (
+            <Box flexDirection="column">
+              <Link to="/home">
+                <Button>Dashboard</Button>
+              </Link>
+              <Link to="/my-account">
+                <Button>Account</Button>
+              </Link>
+              <Button onClick={logout}>Logout</Button>
             </Box>
-        </AppBar >
-    );
+          ) : (
+            <Box>
+              <Link to="/login">
+                <Button>Login</Button>
+              </Link>
+              <Link to="/register">
+                <Button>Register</Button>
+              </Link>
+            </Box>
+          )}
+        </div>
+      </Toolbar>
+      <Box padding={0.5} alignSelf="flex-end">
+        <Chip color="secondary" label={`Account Balance: ${balance}`} />
+        <Chip color="secondary" label={`Max: 1000`} />
+        <Chip color="secondary" label={`Pending: ${pending}`} />
+      </Box>
+    </AppBar>
+  );
 }
 
-
 function mapStateToProps(state) {
-    return {
-        user: state.user
-    };
+  return {
+    user: state.user,
+  };
 }
 
 const mapDispatchToProps = {
-    logout,
-    loadUpdatedWagers
+  logout,
+  loadUpdatedWagers,
 };
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
